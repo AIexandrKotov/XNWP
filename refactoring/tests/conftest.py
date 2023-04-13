48ad66951d28cad972a1974d13e768beb86ccac3
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2023-04-13 21:53:23 +0300

    format,,,

--- refactoring/tests/conftest.py
+++ refactoring/tests/conftest.py
@@ -7,7 +7,27 @@ def profile() -> XNWPProfile:
     return XNWPProfile(
         notes=[("base", [Note(text="Я хочу проверить сработает ли гитхаб экшнс")])],
         persons=[
-            ("base", [Person(properties=[("base", [Property(name="Имя", generator="", icon="", value="Челябинск", generator_arguments={})])])])
+            (
+                "base",
+                [
+                    Person(
+                        properties=[
+                            (
+                                "base",
+                                [
+                                    Property(
+                                        name="Имя",
+                                        generator="",
+                                        icon="",
+                                        value="Челябинск",
+                                        generator_arguments={},
+                                    )
+                                ],
+                            )
+                        ]
+                    )
+                ],
+            )
         ],
         sample_persons=[],
         sample_properties=[],
