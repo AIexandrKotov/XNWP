721a84d13a32af8c91c763b089216b4a2e709372
Kotov <44296606+AIexandrKotov@users.noreply.github.com>
2023-04-13 21:52:06 +0300

    fix test init

--- refactoring/tests/conftest.py
+++ refactoring/tests/conftest.py
@@ -5,9 +5,9 @@ from logic import Note, Person, Property, XNWPProfile
 @pytest.fixture()
 def profile() -> XNWPProfile:
     return XNWPProfile(
-        notes=[("base", [Note("Я хочу проверить сработает ли гитхаб экшнс")])],
+        notes=[("base", [Note(text="Я хочу проверить сработает ли гитхаб экшнс")])],
         persons=[
-            ("base", [Person([("base", [Property("Имя", "", "", "Челябинск", {})])])])
+            ("base", [Person(properties=[("base", [Property(name="Имя", generator="", icon="", value="Челябинск", generator_arguments={})])])])
         ],
         sample_persons=[],
         sample_properties=[],
